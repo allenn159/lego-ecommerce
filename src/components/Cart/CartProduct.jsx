d7e3486468f9a1bff2ce2f0483c7c8e6f21b6dff
allenn159 <allenn159@gmail.com>
2021-08-14 18:31:11 -0700

    Clean up

--- src/components/Cart/CartProduct.jsx
+++ src/components/Cart/CartProduct.jsx
@@ -1,6 +1,8 @@
-import React, { useState, useEffect, useRef } from "react";
+import React, { useState, useCallback } from "react";
 import { TextField, IconButton } from "@material-ui/core";
 import CloseIcon from "@material-ui/icons/Close";
+import AutorenewIcon from "@material-ui/icons/Autorenew";
+
 
 import useStyles from "./styles";
 
@@ -8,33 +10,27 @@ const CartProduct = ({ item, onUpdateCart, onRemoveFromCart }) => {
   const classes = useStyles();
   const [quantity, setQuantity] = useState(item.quantity);
 
-  const initialRender = useRef(true);
-
   const onHandleChange = (event) => {
     setQuantity(event.target.value);
   };
 
-  useEffect(() => {
-    if (initialRender.current) {
-      initialRender.current = false;
-    } else {
-      onUpdateCart(item.id, quantity);
-    }
-  }, [onUpdateCart, item.id, quantity]);
+  const onUpdateQuantity = () => {
+    onUpdateCart(item.id, quantity);
+  };
 
   return (
     <div>
       <div className={classes.lineItems}>
-        <img width="200px" src={item.media.source} alt={item.name} />
-        <p style={{ marginLeft: "100px", width: "950px" }}>
-          {item.product_name}
-        </p>
+        <img
+          className={classes.image}
+          width="200px"
+          src={item.media.source}
+          alt={item.name}
+        />
+        <p className={classes.productName}>{item.product_name}</p>
 
         <TextField
-          style={{
-            marginRight: "30px",
-            width: "125px",
-          }}
+          className={classes.quantityInput}
           size="medium"
           label={`${item.price.formatted} each`}
           type="number"
@@ -42,18 +38,21 @@ const CartProduct = ({ item, onUpdateCart, onRemoveFromCart }) => {
           value={quantity}
           onChange={onHandleChange}
         />
-        <p
-          style={{
-            marginRight: "50px",
-          }}
-        >
+        <IconButton onClick={onUpdateQuantity}>
+          <AutorenewIcon className={classes.renewButton} />
+        </IconButton>
+        <p className={classes.itemTotal}>
           Total: ${item.price.formatted * item.quantity}
         </p>
         <IconButton
           onClick={() => onRemoveFromCart(item.id)}
           aria-label="delete"
         >
-          <CloseIcon style={{ color: "#ff1744" }} />
+          <CloseIcon
+            style={{
+              color: "#ff1744",
+            }}
+          />
         </IconButton>
       </div>
     </div>
--- src/components/Cart/styles.js
+++ src/components/Cart/styles.js
@@ -3,24 +3,28 @@ import { makeStyles } from "@material-ui/core/styles";
 export default makeStyles((theme) => ({
   container: {
     position: "relative",
-    marginTop: "95px",
-    display: "flex",
-    justifyContent: "center",
-    margin: "0 25px",
-  },
-  button: {},
-  paper: {
-    display: "flex",
-    justifyContent: "center",
+    margin: "95px 25px",
   },
   lineItems: {
     display: "flex",
     alignItems: "center",
-    margin: " 0 0 25px 200px",
-    padding: "25px 0",
+    marginBottom: "25px",
     fontFamily: "Poppins",
   },
-  textField: {},
+  image: {
+    marginLeft: "200px",
+    marginRight: "100px",
+  },
+  productName: { width: "200px", marginRight: "600px" },
+  quantityInput: {
+    width: "100px",
+    marginRight: "25px",
+  },
+  renewButton: {
+    fontSize: "30px",
+    color: "#00a152",
+  },
+  itemTotal: { width: "100px", marginLeft: "25px", marginRight: "150px" },
   emptyCart: {
     marginTop: "300px",
     display: "flex",
