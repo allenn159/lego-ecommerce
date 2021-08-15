b6f7988b2e9b11717e82f37b96126dac68fcdf29
allenn159 <allenn159@gmail.com>
2021-08-14 18:41:32 -0700

    Clean up

--- src/components/Cart/CartProduct.jsx
+++ src/components/Cart/CartProduct.jsx
@@ -3,19 +3,23 @@ import { TextField, IconButton } from "@material-ui/core";
 import CloseIcon from "@material-ui/icons/Close";
 import AutorenewIcon from "@material-ui/icons/Autorenew";
 
-
 import useStyles from "./styles";
 
 const CartProduct = ({ item, onUpdateCart, onRemoveFromCart }) => {
   const classes = useStyles();
   const [quantity, setQuantity] = useState(item.quantity);
+  const [prevQuantity, setPrevQuantity] = useState(item.quantity);
 
   const onHandleChange = (event) => {
     setQuantity(event.target.value);
+    setPrevQuantity(quantity);
   };
 
   const onUpdateQuantity = () => {
-    onUpdateCart(item.id, quantity);
+    if (quantity != prevQuantity) {
+      onUpdateCart(item.id, quantity);
+      setPrevQuantity(quantity);
+    }
   };
 
   return (
